46aeaf360dc2736784c27dddfe2f6360bc50ba7d
Al Mustarik <almustarik@gmail.com>
2021-01-06 01:21:42 +0600

    like & unlike functionality added

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, \
+    AddCategoryView, CategoryView, CategoryListView, LikeView
 
 urlpatterns = [
     # path('', views.home, name="home"),
@@ -9,4 +10,8 @@ urlpatterns = [
     path('add_post/', AddPostView.as_view(), name="add_post"),
     path('article/edit/<int:pk>', UpdatePostView.as_view(), name="update-post"),
     path('article/<int:pk>/remove', DeletePostView.as_view(), name="delete-post"),
+    path('add_category/', AddCategoryView.as_view(), name="add_category"),
+    path('category/<str:cats>/', CategoryView, name="category"),
+    path('category-list', CategoryListView, name="category-list"),
+    path('like/<int:pk>', LikeView, name='like_post')
 ]
