56023f322cda4df2d8d5aa517a7331d5941e9f58
Al Mustarik <almustarik@gmail.com>
2021-01-07 21:02:36 +0600

    added image upload functionality

--- members/views.py
+++ members/views.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.views import PasswordChangeView
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.urls import reverse_lazy
-from members.forms import SignUpForm
+from members.forms import SignUpForm, EditProfileForm, PasswordChangingForm
 
 
 # Create your views here.
@@ -10,3 +11,23 @@ class UserRegisterView(generic.CreateView):
     form_class = SignUpForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
+
+
+class UserEditView(generic.UpdateView):
+    form_class = EditProfileForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self):
+        return self.request.user
+
+
+class PasswordsChangeView(PasswordChangeView):
+    form_class = PasswordChangingForm
+    # form_class = PasswordChangeForm
+    # success_url = reverse_lazy('home')
+    success_url = reverse_lazy('password_success')
+
+
+def password_success(request):
+    return render(request, 'registration/password_success.html', {})
--- myblog/models.py
+++ myblog/models.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from datetime import datetime, date
 from ckeditor.fields import RichTextField
 
+
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -16,14 +17,24 @@ class Category(models.Model):
         return reverse('home')
 
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    bio = models.TextField()
+
+    def __str__(self):
+        return str(self.user)
+
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
+    header_image = models.ImageField(null=True, blank=True, upload_to="images/")
     title_tag = models.CharField(max_length=255, default="My Freaking Awesome Blog")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     # body = models.TextField()
     body = RichTextField(blank=True, null=True)
     post_date = models.DateField(auto_now_add=True)
     category = models.CharField(max_length=255, default="coding")
+    snippet = models.CharField(max_length=255)
     likes = models.ManyToManyField(User, related_name='blog_posts')
 
     def total_likes(self):
