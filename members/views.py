1ece1b026e8d57fd4645b6adf1f577c64e9bf4a9
Al Mustarik <almustarik@gmail.com>
2021-01-09 00:54:49 +0600

    added profile page

--- members/views.py
+++ members/views.py
@@ -1,12 +1,28 @@
 from django.contrib.auth.views import PasswordChangeView
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.urls import reverse_lazy
 from members.forms import SignUpForm, EditProfileForm, PasswordChangingForm
+from django.views.generic import DetailView
+from myblog.models import UserProfile
 
 
 # Create your views here.
+class ShowProfilePageView(DetailView):
+    model = UserProfile
+    template_name = 'registration/user_profile.html'
+
+    def get_context_data(self, *args, **kwargs):
+        # users = UserProfile.objects.all()
+        context = super(ShowProfilePageView, self).get_context_data(*args, **kwargs)
+
+        page_user = get_object_or_404(UserProfile, id=self.kwargs['pk'])
+
+        context["page_user"] = page_user
+        return context
+
+
 class UserRegisterView(generic.CreateView):
     form_class = SignUpForm
     template_name = 'registration/register.html'
--- myblog/models.py
+++ myblog/models.py
@@ -20,6 +20,12 @@ class Category(models.Model):
 class UserProfile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     bio = models.TextField()
+    profile_pic = models.ImageField(null=True, blank=True, upload_to="images/profile/")
+    website_url = models.CharField(max_length=255, null=True, blank=True)
+    facebook_url = models.CharField(max_length=255, null=True, blank=True)
+    instagram_url = models.CharField(max_length=255, null=True, blank=True)
+    linkedin_url = models.CharField(max_length=255, null=True, blank=True)
+    github_url = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return str(self.user)
